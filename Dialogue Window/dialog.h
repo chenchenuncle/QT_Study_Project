a759a33ff59a0168e8380c41f9826f64cbffe193
阿叔 <159232555+chenchenuncle@users.noreply.github.com>
2026-02-02 19:09:08 +0800

    Add files via upload

---
+++ Dialogue Window/dialog.h
@@ -0,0 +1,62 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Dialog;
+}
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+private slots:
+    void do_sta(bool=true);
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_14_clicked();
+
+    void on_pushButton_15_clicked();
+
+    void on_pushButton_16_clicked();
+
+    void on_pushButton_17_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+private:
+    Ui::Dialog *ui;
+};
+#endif // DIALOG_H
